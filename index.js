84c1cec3cd650f418f846e153e0d57540360ec5d
koushiki2001 <rupai.koushiki01@gmail.com>
2022-04-13 20:27:04 +0530

    Places Autocomplete done

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 const express = require('express');
 const bodyparser = require('body-parser');
-
+const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require('path')
+const ejs = require("ejs");
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+const uri = `mongodb+srv://parkonthego:parkonthego@cluster0.2uafm.mongodb.net/parkonthegoDB?retryWrites=true&w=majority`;
+
+mongoose.connect(uri, { useUnifiedTopology: true } 
+    );
+
+const connection = mongoose.connection;
+connection.once('open', () =>{
+    console.log("MongoDB database connection successfully established");
+});
+
+app.get('/',function(req,res){
+    res.render("home"); 
+  });
+
+  app.get('/getDestination',function(req,res){
+    res.render("destination"); 
+  });
+  app.post('/destination',function(req,res){
+      console.log(req.body);
+  })
 
 app.listen(port,()=>{
     console.log(`Server running on port ${port}`);
---
+++ public/map.js
@@ -0,0 +1,88 @@
+// This example uses the autocomplete feature of the Google Places API.
+// It allows the user to find all hotels in a given place, within a given
+// country. It then displays markers for all the hotels returned,
+// with on-click details for each hotel.
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+let autocomplete;
+
+
+const countries = {
+  au: {
+    center: { lat: -25.3, lng: 133.8 },
+    zoom: 4,
+  },
+
+  in: {
+    center: { lat: 20.59, lng: 78.96 },
+    zoom: 4,
+  },
+
+  br: {
+    center: { lat: -14.2, lng: -51.9 },
+    zoom: 3,
+  },
+  ca: {
+    center: { lat: 62, lng: -110.0 },
+    zoom: 3,
+  },
+  fr: {
+    center: { lat: 46.2, lng: 2.2 },
+    zoom: 5,
+  },
+  de: {
+    center: { lat: 51.2, lng: 10.4 },
+    zoom: 5,
+  },
+  mx: {
+    center: { lat: 23.6, lng: -102.5 },
+    zoom: 4,
+  },
+  nz: {
+    center: { lat: -40.9, lng: 174.9 },
+    zoom: 5,
+  },
+  it: {
+    center: { lat: 41.9, lng: 12.6 },
+    zoom: 5,
+  },
+  za: {
+    center: { lat: -30.6, lng: 22.9 },
+    zoom: 5,
+  },
+  es: {
+    center: { lat: 40.5, lng: -3.7 },
+    zoom: 5,
+  },
+  pt: {
+    center: { lat: 39.4, lng: -8.2 },
+    zoom: 6,
+  },
+  us: {
+    center: { lat: 37.1, lng: -95.7 },
+    zoom: 3,
+  },
+  uk: {
+    center: { lat: 54.8, lng: -4.6 },
+    zoom: 5,
+  },
+};
+
+function initMap() {
+  
+
+  autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById("autocomplete"),
+    {
+      types: ["address"],
+      fields: ["geometry"],
+    }
+  );
+
+
+
+  
+}
+
