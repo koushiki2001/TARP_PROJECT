83f8d3c16436ac7536b5bbb8e8feef817e314493
koushiki2001 <rupai.koushiki01@gmail.com>
2022-04-13 19:38:12 +0530

    Server created and nodemon installed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const express = require('express');
+const bodyparser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}`);
+})
